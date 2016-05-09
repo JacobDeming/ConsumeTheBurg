7104328242fa764800dd1592f8a063216894cb50
Jacob Deming <Jacob Deming>
2016-05-09 16:11:01 -0400

    Pushing to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'LARPing1991',
-    database: 'burgers_db'
+    host: 'd5x4ae6ze2og6sjo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'zim8ou3ozhrdmf3n',
+    password: 'wpez4i0sq83rtfd4',
+    database: 'pf0hwt54gj12cb0a'
 });
 
 connection.connect(function(err){
